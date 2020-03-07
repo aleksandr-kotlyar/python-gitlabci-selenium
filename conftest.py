76188706c47ce6c36182b916c1ff7e4fa3638428
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2020-03-08 02:26:23 +0300

    Init __version__=0.1.1 fix Remote: throw chrome Options() to Remote webdriver

--- conftest.py
+++ conftest.py
@@ -22,11 +22,13 @@
 
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 @pytest.fixture(scope='function')
 def browser() -> WebDriver:
     driver = webdriver.Remote(
-        command_executor='http://selenium__standalone-chrome:4444/wd/hub')
+        command_executor='http://selenium__standalone-chrome:4444/wd/hub',
+        options=Options())
     return driver
